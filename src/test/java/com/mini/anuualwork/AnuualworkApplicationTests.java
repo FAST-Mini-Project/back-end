d0a5616db9e0f7c92f26578621f8f98e992bc341
khsrla9806 <kimhunsope@naver.com>
2023-07-25 15:43:28 +0900

    init: 프로젝트 초기 세팅
    - Spring Security 기능 비활성화
    - application.yml 설정

---
+++ src/test/java/com/mini/anuualwork/AnuualworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.anuualwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnuualworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
