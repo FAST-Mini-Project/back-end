76332ef69840144de9fb57db711dbecb297dc68d
khsrla9806 <kimhunsope@naver.com>
2023-07-28 20:10:27 +0900

    [#19] feat: 현재 연도와 최대 연차 개수를 파라미터로 받아서 JPQL를 처리하도록 수정
    - 동적으로 변할 수 있는 부분들을 파라미터로 분리했습니다.

--- src/main/java/com/mini/anuualwork/service/AdminService.java
+++ src/main/java/com/mini/anuualwork/service/AdminService.java
@@ -6,16 +6,21 @@ import com.mini.anuualwork.repository.AdminMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class AdminService {
+    private static final Long TOTAL_ANNUAL_COUNT = 15L;
 
     private final AdminMemberRepository adminMemberRepository;
 
     public ApiDataResponse<?> getAllMembers() {
-        List<AdminDto.RequestAllMembers> allMembers = adminMemberRepository.getAllMembersWithAnnualCountAndWorkCount();
+        int year = LocalDateTime.now().getYear();
+
+        List<AdminDto.RequestAllMembers> allMembers =
+                adminMemberRepository.getAllMembersWithAnnualCountAndWorkCount(TOTAL_ANNUAL_COUNT, year);
 
         return new ApiDataResponse<>(allMembers);
     }
