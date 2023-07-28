59ff76b9bc114039e038a4900d53448c6b823997
khsrla9806 <kimhunsope@naver.com>
2023-07-28 20:10:27 +0900

    [#19] feat: 현재 연도와 최대 연차 개수를 파라미터로 받아서 JPQL를 처리하도록 수정
    - 동적으로 변할 수 있는 부분들을 파라미터로 분리했습니다.

--- src/main/java/com/mini/anuualwork/repository/AdminMemberRepository.java
+++ src/main/java/com/mini/anuualwork/repository/AdminMemberRepository.java
@@ -4,6 +4,7 @@ import com.mini.anuualwork.dto.AdminDto;
 import com.mini.anuualwork.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,9 +12,18 @@ import java.util.List;
 public interface AdminMemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m.id AS id, m.name AS name, m.email AS email, m.employeeNumber AS employeeNumber, " +
-            "(SELECT 15 - COUNT(a) FROM Annual a WHERE a.member.id = m.id AND a.status != 'CANCELED') AS restAnnual, " +
-            "(SELECT COUNT(w) FROM Work w WHERE w.member.id = m.id) AS workDay " +
+            "(" +
+            "   SELECT :totalAnnualCount - COUNT(a) " +
+            "   FROM Annual a " +
+            "   WHERE a.member.id = m.id AND a.status != 'CANCELED' AND YEAR(a.date) = :year" +
+            ") AS restAnnual, " +
+            "(" +
+            "   SELECT COUNT(w) " +
+            "   FROM Work w " +
+            "   WHERE w.member.id = m.id AND YEAR(w.date) = :year" +
+            ") AS workDay " +
             "FROM Member m " +
             "ORDER BY m.id ASC")
-    List<AdminDto.RequestAllMembers> getAllMembersWithAnnualCountAndWorkCount();
+    List<AdminDto.RequestAllMembers> getAllMembersWithAnnualCountAndWorkCount(
+            @Param("totalAnnualCount") Long totalAnnualCount, @Param("year") Integer year);
 }
