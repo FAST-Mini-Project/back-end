7d657a74e2a57553589e4a7794beffea230c261e
khsrla9806 <kimhunsope@naver.com>
2023-07-28 19:43:32 +0900

    [#19] feat: AdminDto 생성 후 JPQL Projection 결과를 담을 interface 구현
    - JPQL이나 Native Query에서 집계 쿼리를 사용하면 Object 타입으로 반환
    - 원하는 데이터 타입에 Projection 결과를 담기 위해서는 interface + getter를 사용

---
+++ src/main/java/com/mini/anuualwork/dto/AdminDto.java
@@ -0,0 +1,14 @@
+package com.mini.anuualwork.dto;
+
+public class AdminDto {
+
+    /* 집계 쿼리가 포함된 JPQL Projection 결과를 Converting 하기 위해서는 interface + Getter 사용 */
+    public interface RequestAllMembers {
+        Long getId();
+        String getName();
+        String getEmail();
+        String getEmployeeNumber();
+        Integer getRestAnnual();
+        Integer getWorkDay();
+    }
+}
