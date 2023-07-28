e39820022895d88d792638eecb0c3597ec853979
khsrla9806 <kimhunsope@naver.com>
2023-07-28 15:11:44 +0900

    [#14] refactor: 공통 응답 리펙토링
    - 멘토링 의견 수렴하여 statusCode와 errorCode를 제거했습니다.

--- src/main/java/com/mini/anuualwork/core/ApiDataResponse.java
+++ src/main/java/com/mini/anuualwork/core/ApiDataResponse.java
@@ -1,16 +1,13 @@
 package com.mini.anuualwork.core;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
-/** 정상 응답 클래스 */
+/* 정상 응답 클래스 */
 @Getter
 public class ApiDataResponse<T> {
-    private final Integer statusCode;
     private final T data;
 
-    public ApiDataResponse(T data, HttpStatus status) {
-        this.statusCode = status.value();
+    public ApiDataResponse(T data) {
         this.data = data;
     }
 }
