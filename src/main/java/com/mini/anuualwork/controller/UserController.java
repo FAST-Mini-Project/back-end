e72bcf77fb0dcec19b7f42d001bea181e2c85623
KH <kkh96071@gmail.com>
2023-07-31 18:56:00 +0900

    [#27] feature: 로그아웃 구현 완료
    로그아웃시, 헤더에 담겨진 토큰의 유효기간을 과거로 되돌린다.
    
    유효기간이 지난 토큰을 재사용 할 순 없으므로, 새로운 토큰을 만들고 사용해야 한다.

--- src/main/java/com/mini/anuualwork/controller/UserController.java
+++ src/main/java/com/mini/anuualwork/controller/UserController.java
@@ -2,10 +2,12 @@ package com.mini.anuualwork.controller;
 
 import com.mini.anuualwork.core.ApiDataResponse;
 import com.mini.anuualwork.dto.LoginDto;
+import com.mini.anuualwork.dto.LogoutDto;
 import com.mini.anuualwork.dto.SignupDto;
 import com.mini.anuualwork.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +33,12 @@ public class UserController {
     public ApiDataResponse<SignupDto.ResponseSignupSuccess> signup(@RequestBody SignupDto signupDto){
        return userService.join(signupDto);
     }
+
+    @RequestMapping(value = "/api/logout", method = RequestMethod.POST)
+    public ApiDataResponse<LogoutDto> logout(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+        String token = authorization.split(" ")[1];
+        return userService.logout(token);
+    }
+
 
 }
