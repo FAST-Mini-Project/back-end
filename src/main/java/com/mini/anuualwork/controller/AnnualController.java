e539ebef50917147a3b06069fbe1fd2fd90dfe2f
GyeongMin <aklmo3778@naver.com>
2023-08-02 14:03:49 +0900

    [#32] feat: 개인 연차 조회 기능 구현

--- src/main/java/com/mini/anuualwork/controller/AnnualController.java
+++ src/main/java/com/mini/anuualwork/controller/AnnualController.java
@@ -2,16 +2,12 @@ package com.mini.anuualwork.controller;
 
 import com.mini.anuualwork.core.ApiDataResponse;
 import com.mini.anuualwork.dto.AnnualDto;
-import com.mini.anuualwork.entity.Work;
 import com.mini.anuualwork.service.AnnualService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +19,12 @@ public class AnnualController {
     @GetMapping("/api/schedule/annual")
     public ApiDataResponse<List<AnnualDto.AnnualInfoResponse>> getAnnualInfoList(@RequestParam Integer year, @RequestParam Integer month) {
         return annualService.getAnnualInfoList(year, month);
+    }
+
+    //개인 연차 조회
+    @GetMapping("/api/user/annual")
+    public ApiDataResponse<?> getMemberAnnualList(Authentication authentication, @RequestParam Integer year){
+        return annualService.getMemberAnnualList(authentication.getName(), year);
     }
 
 }
