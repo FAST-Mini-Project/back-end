42bb56201a37e84366edaee9d6800b521d1d3634
KH <kkh96071@gmail.com>
2023-08-02 17:56:53 +0900

    [#37] refactor: 로그인 EmployeeNumber, cors 설정 리팩토링
    로그인 시 employeeNumber을 4자리로 끊어서 줌.
    
    cors 설정 -> 프론트가 접근 가능하도록 변경.

--- src/main/java/com/mini/anuualwork/config/AuthenticationConfig.java
+++ src/main/java/com/mini/anuualwork/config/AuthenticationConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +35,8 @@ public class AuthenticationConfig {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
-                .cors().and()
+                .cors().configurationSource(configurationSource())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/login", "/api/signup").permitAll() //login은 모든 접근 허용함
 //                .antMatchers(HttpMethod.POST,"/api/**").authenticated()
@@ -44,4 +48,20 @@ public class AuthenticationConfig {
                 //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻
                 .build();
     }
+
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        // localhost:8080 백엔드, localhost:3000 프론트엔드
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+        configuration.addExposedHeader("Authorization"); // 옛날에는 디폴트 였다. 지금은 아닙니다.
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // /login, /board, /product/
+        return source;
+    }
+
+
 }
+
