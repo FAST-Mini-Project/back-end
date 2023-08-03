9ad124020903eb5c63237cecb09e28137e4150e7
GyeongMin <aklmo3778@naver.com>
2023-08-03 16:44:48 +0900

    [#34] feat: 개인 연차 신청 기능 구현

--- src/main/java/com/mini/anuualwork/controller/AnnualController.java
+++ src/main/java/com/mini/anuualwork/controller/AnnualController.java
@@ -1,13 +1,22 @@
 package com.mini.anuualwork.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mini.anuualwork.core.ApiDataResponse;
 import com.mini.anuualwork.dto.AnnualDto;
 import com.mini.anuualwork.service.AnnualService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +34,12 @@ public class AnnualController {
     @GetMapping("/api/user/annual")
     public ApiDataResponse<?> getMemberAnnualList(Authentication authentication, @RequestParam Integer year){
         return annualService.getMemberAnnualList(authentication.getName(), year);
+    }
+
+    //연차 등록 신청
+    @PostMapping("/api/schedule/annual")
+    public ApiDataResponse<?> requestAnnual(Authentication authentication, @RequestBody String request) throws ParseException {;
+        return annualService.requestAnnual(authentication.getName(), request);
     }
 
 }
--- src/main/java/com/mini/anuualwork/dto/AnnualDto.java
+++ src/main/java/com/mini/anuualwork/dto/AnnualDto.java
@@ -1,9 +1,16 @@
 package com.mini.anuualwork.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mini.anuualwork.entity.Member;
 import com.mini.anuualwork.entity.type.AnnualStatus;
 import lombok.Data;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class AnnualDto {
 
@@ -18,6 +25,19 @@ public class AnnualDto {
         Long getAnnualId();
         Date getDate();
         AnnualStatus getStatus();
+    }
+
+    @Data
+    public static class AnnualResponse{
+        private final String message;
+    }
+
+    @Data
+    public static class AnnualDate{
+
+        private LocalDate date;
+        private AnnualStatus status;
+        private Member member;
     }
 
 }
