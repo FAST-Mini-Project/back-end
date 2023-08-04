28f36675a0b5aa1b9e4afa5619bc859d3d45c546
GyeongMin <aklmo3778@naver.com>
2023-08-04 13:08:28 +0900

    [#34] refactor: 개인 연차 등록 코드 수정

--- src/main/java/com/mini/anuualwork/service/AnnualService.java
+++ src/main/java/com/mini/anuualwork/service/AnnualService.java
@@ -8,20 +8,10 @@ import com.mini.anuualwork.entity.type.AnnualStatus;
 import com.mini.anuualwork.repository.AnnualRepository;
 import com.mini.anuualwork.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
-import java.util.StringTokenizer;
 
 @Service
 @RequiredArgsConstructor
@@ -43,29 +33,13 @@ public class AnnualService {
     }
 
     @Transactional
-    public ApiDataResponse<?> requestAnnual(String email, String request) throws ParseException {
-        //json 파싱 -> string을 date로 변환
-        String data = null;
-
-        StringTokenizer st = new StringTokenizer(request, "{:}");
-        while(st.hasMoreTokens() && !st.nextToken().equals("}")){
-            data = st.nextToken();
-        }
-
-        data = data.replaceAll("\"", "");
-        data = data.replaceAll(" ", "");
-        data = data.replaceAll("\n", "");
-        data += "T00:00:00";
-
-        LocalDateTime date = LocalDateTime.parse(data);
-
+    public ApiDataResponse<?> requestAnnual(String email, AnnualDto.AnnualDate annualDate) {
         Member member = memberRepository.findMemberByEmail(email);
 
-        Annual annual = new Annual();
-        annual.setStatus(AnnualStatus.UNAPPROVED);
-        annual.setDate(date);
-
+        Annual annual = annualDate.toEntity();
         annual.setMember(member);
+        annual.setStatus(AnnualStatus.UNAPPROVED);
+
         annualRepository.save(annual);
 
         return new ApiDataResponse<>(new AnnualDto.AnnualResponse("연차 등록에 성공했습니다."));
