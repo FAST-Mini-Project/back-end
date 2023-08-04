e8e0bcc7e94d84bcb367984627de7e951302a9c4
khsrla9806 <kimhunsope@naver.com>
2023-08-04 14:07:20 +0900

    [#44] fix: GlobalExceptionHandler에서 적절한 Http 상태코드를 반환하도록 설정
    - 기존에는 Error인데도 200 OK를 발생
    - 적절하게 400 BADREQUEST가 발생하도록 수정 (추후 커스텀 Exception 리펙토링 계획)

--- src/main/java/com/mini/anuualwork/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mini/anuualwork/exception/handler/GlobalExceptionHandler.java
@@ -2,12 +2,15 @@ package com.mini.anuualwork.exception.handler;
 
 import com.mini.anuualwork.core.ApiErrorResponse;
 import com.mini.anuualwork.exception.UserException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ApiErrorResponse handleException(Exception exception) {
         String[] errorMessages = {exception.getMessage()};
@@ -15,9 +18,11 @@ public class GlobalExceptionHandler {
         return new ApiErrorResponse(errorMessages);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserException.class)
     public ApiErrorResponse handleUserException(UserException userException) {
         String[] errorMessages = {userException.getMessage()};
+
         return new ApiErrorResponse(errorMessages);
     }
 
