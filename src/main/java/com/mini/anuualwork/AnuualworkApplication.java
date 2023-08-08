b2815db2e4f6b4c24d0ed143b403ea26421b8438
khsrla9806 <kimhunsope@naver.com>
2023-08-08 11:52:57 +0900

    [#50] refactor: 작업 초기에 추가해뒀던 Security 제외 코드 제거

--- src/main/java/com/mini/anuualwork/AnuualworkApplication.java
+++ src/main/java/com/mini/anuualwork/AnuualworkApplication.java
@@ -2,9 +2,8 @@ package com.mini.anuualwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class AnuualworkApplication {
 
 	public static void main(String[] args) {
