4e9723dc73fb98e2de3006a9361efce412481a45
khsrla9806 <kimhunsope@naver.com>
2023-08-08 16:56:52 +0900

    [#58] feat: 관리자 당직 등록 유효성 검사 로직 구현
    - 이미 해당 날짜에 해당 유저의 당직이 존재하는지 확인
    - 이미 해당 날짜에 유저가 연차를 신청하거나, 신청 상태인지 확인

--- src/main/java/com/mini/anuualwork/exception/message/AdminErrorMessage.java
+++ src/main/java/com/mini/anuualwork/exception/message/AdminErrorMessage.java
@@ -3,6 +3,8 @@ package com.mini.anuualwork.exception.message;
 public interface AdminErrorMessage {
     String NOT_FOUND_MEMBER = "존재하지 않는 유저입니다.";
     String NOT_FOUND_WORK = "존재하지 않는 당직 정보입니다.";
+    String ALREADY_EXIST_WORK = "이미 해당 날짜에 당직이 등록된 유저입니다.";
     String NOT_FOUND_ANNUAL = "존재하지 않는 연차 정보입니다.";
     String ALREADY_APPROVED_ANNUAL = "이미 승인된 연차 정보입니다.";
+    String ALREADY_EXIST_ANNUAL = "이미 해당 날짜에 연차 또는 신청 내역이 존재합니다. 해당 날짜의 연차를 확인해주세요.";
 }
