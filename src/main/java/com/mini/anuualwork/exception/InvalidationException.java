4c4dcc2a9308ce75d48a25e03133c33e13b58c99
khsrla9806 <kimhunsope@naver.com>
2023-08-08 13:21:15 +0900

    [#52] feat: Validation 관련 Exception 추가 후 handler 메서드 추가
    - Exception에서 유효성 검사에 대한 FieldError를 받아서 저장

---
+++ src/main/java/com/mini/anuualwork/exception/InvalidationException.java
@@ -0,0 +1,31 @@
+package com.mini.anuualwork.exception;
+
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+public class InvalidationException extends RuntimeException {
+
+    private List<FieldError> fieldErrors;
+
+    public InvalidationException() {
+    }
+
+    public InvalidationException(List<FieldError> fieldErrors) {
+        this.fieldErrors = fieldErrors;
+    }
+
+    public InvalidationException(String message) {
+        super(message);
+    }
+
+    public InvalidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidationException(Throwable cause) {
+        super(cause);
+    }
+}
